ecfee7c12ef510e2a9d9c2a45dd46a72b7b733f2
“CSO147” <olasunkanmi01471977@gmail.com>
2023-04-06 10:44:44 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: string given
- * Return: 0.
+ * Return: Always 0.
  */
 
 int _strlen_recursion(char *s)
@@ -11,5 +11,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return (1 + _strlen_recursion(v + 1));
+		return (1 + _strlen_recursion(s + 1));
 }

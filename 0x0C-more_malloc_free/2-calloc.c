2a2ef31f6b24a2382cc1f727438576894ad3a48a
“CSO147” <olasunkanmi01471977@gmail.com>
2023-04-14 00:35:43 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  * _calloc - functions allocates some memory for an array.
- * @nmemb: number of elements.
- * @size: size of bytes.
+ * @nmemb: the number of elements.
+ * @size: the size of bytes.
  *
  * Return: pointer to the allocated memory.
  * if nmemb or size is 0, returns NULL.
@@ -11,7 +11,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *c;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
